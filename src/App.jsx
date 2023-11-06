b9ff4d62b924065e5f9e216c4f774e77db0e73a8
unknown <Arunkumaronedata>
2023-11-06 19:17:17 +0530

    add and delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,75 @@
 import React, { useState } from 'react';
 import DragDrop from './components/DrgaDrop';
+import deletedItem from '../src/assets/delete.png';
+import addItem from '../src/assets/add-item.png'
+
 
 function App() {
-  const [showDragDrop, setShowDragDrop] = useState(false);
+  const card = {
+    width: '70vw',
+    height: '70vh',
+    padding: '20px 20px',
+    margin: '5vh 10vw',
+  };
 
-  const handleButtonClick = () => {
-    setShowDragDrop(true);
+  const [boxes, setBoxes] = useState([]);
+  const [idCounter, setIdCounter] = useState(0);
+  const [selectedBoxId, setSelectedBoxId] = useState(null);
+
+  const addResizableBox = () => {
+    const newBoxes = {
+      id: idCounter,
+      left: '0',
+    };
+    setIdCounter(idCounter + 1);
+
+    setBoxes([...boxes, newBoxes]);
+  };
+
+  const deleteBox = () => {
+    if (selectedBoxId !== null) {
+      const updatedBoxes = boxes.filter((box) => box.id !== selectedBoxId);
+      setBoxes(updatedBoxes);
+      setSelectedBoxId(null); // Clear the selected item
+    }
   };
 
   return (
-    <div>
-      <button onClick={handleButtonClick}>Click</button>
-      {showDragDrop && <DragDrop />}
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card" style={card}>
+            <div className="card-body">
+              <div className='sidebar d-flex'>
+                <div className='col-md-1'>
+                  <div className="card addBox1" style={{ height: '60vh' }}>
+                    <div className="card-body">
+                      {/* <button onClick={addResizableBox}>Add</button> */}
+                      <img  onClick={addResizableBox} src={addItem} alt="add" style={{width:'30px'}}/>
+                      <img onClick={deleteBox} src={deletedItem} alt="delete" style={{width:'30px', marginTop:'10px'}}/> {/* Delete button in addBox1 */}
+                    </div>
+                  </div>
+                </div>
+                <div className='col-md-11'>
+                  <div className="card addBox2" style={{ height: '60vh', position: 'relative' }} >
+                    <div className="card-body">
+                      {boxes.map((box) => (
+                        <div
+                          key={box.id}
+                          style={{ position: 'absolute', left: box.left }}
+                          onClick={() => setSelectedBoxId(box.id)} // Select the item
+                        >
+                          <DragDrop element={box} />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/DrgaDrop.jsx
+++ src/components/DrgaDrop.jsx
@@ -3,6 +3,7 @@ import "./DragDrop.css";
 
 const DragDrop = () => {
   const divRef = useRef(null);
+  const parentRef = useRef(null);
   const [resizing, setResizing] = useState(false);
   const [dragging, setDragging] = useState(false);
   const [startX, setStartX] = useState(0);
@@ -11,60 +12,95 @@ const DragDrop = () => {
   const [startTop, setStartTop] = useState(0);
   const [startWidth, setStartWidth] = useState(0);
   const [startHeight, setStartHeight] = useState(0);
+  const [showHandles, setShowHandles] = useState(false);
+  const [boxes, setBoxes] = useState([]);
 
   useEffect(() => {
     const handleMouseMove = (e) => {
-      if (resizing) {
-        const offsetX = e.clientX - startX;
-        const offsetY = e.clientY - startY;
-
-        const div = divRef.current;
-        const newWidth = startWidth + offsetX;
-        const newHeight = startHeight + offsetY;
-
-        div.style.width = `${newWidth}px`;
-        div.style.height = `${newHeight}px`;
-      }
       if (dragging) {
         const offsetX = e.clientX - startX;
         const offsetY = e.clientY - startY;
-
+  
         const div = divRef.current;
-        const newLeft = startLeft + offsetX;
-        const newTop = startTop + offsetY;
-
+        let newLeft = startLeft + offsetX;
+        let newTop = startTop + offsetY;
+  
+        if (newLeft < 0) {
+          newLeft = 0;
+        }
+        if (newTop < 0) {
+          newTop = 0;
+        }
+  
         div.style.left = `${newLeft}px`;
         div.style.top = `${newTop}px`;
       }
+  
+      if (resizing) {
+        const offsetX = e.clientX - startX;
+        const offsetY = e.clientY - startY;
+  
+        const div = divRef.current;
+        let newWidth = startWidth + offsetX;
+        let newHeight = startHeight + offsetY;
+  
+        if (newWidth < 100) {
+          newWidth = 100;
+        }
+        if (newHeight < 100) {
+          newHeight = 100;
+        }
+  
+        div.style.width = `${newWidth}px`;
+        div.style.height = `${newHeight}px`;
+      }
     };
-
+  
     const handleMouseUp = () => {
       setResizing(false);
       setDragging(false);
     };
-
+  
     if (resizing || dragging) {
       document.addEventListener("mousemove", handleMouseMove);
       document.addEventListener("mouseup", handleMouseUp);
     }
-
+  
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
       document.removeEventListener("mouseup", handleMouseUp);
     };
   }, [resizing, dragging, startX, startY, startLeft, startTop, startWidth, startHeight]);
+  
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (divRef.current && !divRef.current.contains(e.target)) {
+        setShowHandles(false);
+        setDragging(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   const handleMouseDown = (e) => {
     const target = e.target;
+    const isResizeHandle = target.classList.contains("resize-handle");
 
-    if (target.classList.contains("resize-handle")) {
+    if (isResizeHandle) {
       setResizing(true);
+      setDragging(false);
       setStartX(e.clientX);
       setStartY(e.clientY);
       setStartWidth(divRef.current.clientWidth);
       setStartHeight(divRef.current.clientHeight);
     } else {
       setDragging(true);
+      setResizing(false);
       setStartX(e.clientX);
       setStartY(e.clientY);
       const div = divRef.current;
@@ -73,35 +109,79 @@ const DragDrop = () => {
     }
   };
 
+
+
+  const handleHideShow = () => {
+    setShowHandles(!showHandles);
+  };
+
+  // const addResizableBox = () => {
+  //   setBoxes([...boxes, <DragDrop key={boxes.length} />]);
+  // };
+
   return (
-    <div className="App" style={{ width: "100vw", height: "100vh", position: "relative" }}>
+    <div
+      className="App"
+      onClick={handleHideShow}
+      style={{ width: "100vw", height: "10vh", position: "relative" }}
+    >
       <div
-        className="resizable-div"
-        ref={divRef}
-        onMouseDown={handleMouseDown}
-        style={{
-          wordBreak: 'break-all',
-          cursor: 'text',
-          border: '1px solid black',
-          width: '100px',
-          height: '100px',
-          whiteSpace: 'pre-wrap',
-          fontSize: '20px',
-          overflow: 'hidden',
-          lineHeight: '30px',
-          userSelect: 'text', // Add userSelect property
-        }}
+        className="resizable-parent"
+        ref={parentRef}
+        style={{ position: "relative", width: "100%", height: "5%" }}
       >
-        <div className="flex-item" contentEditable={true} style={{ overflow: "hidden", maxHeight: "100%", maxWidth: "100%" }}>
-          Hello World
+        <div
+          className="resizable-div"
+          ref={divRef}
+          onMouseDown={handleMouseDown}
+          style={{
+            wordBreak: "break-all",
+            cursor: "text",
+            border: "1px solid black",
+            width: "100px",
+            height: "100px",
+            whiteSpace: "pre-wrap",
+            fontSize: "20px",
+            overflow: "hidden",
+            lineHeight: "30px",
+            userSelect: "text",
+            position: "absolute",
+            left: '0px'
+          }}
+        >
+          <div
+            className="flex-item"
+            contentEditable={true}
+            style={{
+              overflow: "hidden",
+              maxHeight: "100%",
+              maxWidth: "100%",
+            }}
+          >
+            Hello World
+          </div>
+          <div
+            className="flex-item"
+            contentEditable={true}
+            style={{
+              overflow: "hidden",
+              maxHeight: "100%",
+              maxWidth: "100%",
+              marginTop: "100px",
+              alignItems: "center",
+            }}
+          >
+            Register Here
+          </div>
+          {showHandles && (
+            <>
+              <div className="resize-handle top" />
+              <div className="resize-handle right" />
+              <div className="resize-handle bottom" />
+              <div className="resize-handle left" />
+            </>
+          )}
         </div>
-        <div className="flex-item" contentEditable={true} style={{ overflow: "hidden", maxHeight: "100%", maxWidth: "100%", marginTop: '100px', alignItems: 'center' }}>
-          Register Here
-        </div>
-        <div className="resize-handle top" />
-        <div className="resize-handle right" />
-        <div className="resize-handle bottom" />
-        <div className="resize-handle left" />
       </div>
     </div>
   );
